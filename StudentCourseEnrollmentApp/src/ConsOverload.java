c83fa2de1a7f0141426d8cd7e6159554d7b1753a
Kani1997 <117250766+Kani1997@users.noreply.github.com>
2023-01-11 10:22:46 +0530

    Add files via upload

---
+++ StudentCourseEnrollmentApp/src/ConsOverload.java
@@ -0,0 +1,17 @@
+// Constructor overloading
+public class ConsOverload {
+
+		private String name;
+		
+		// method overloding
+		public ConsOverload(String n) {
+			name=n;
+		}
+		public ConsOverload() {
+			name="\t\tEnrollment App";
+			
+		}
+		public void printName() {
+			System.out.println(name);
+		}
+}
---
+++ StudentCourseEnrollmentApp/src/overriding.java
@@ -0,0 +1,15 @@
+
+public class overriding{
+   public void myMethod()
+   {
+	System.out.println("***************************************************");
+   }	   
+}
+
+//Inheritance
+class Demo extends overriding{
+   public void myMethod(){
+	//Super keyword
+	super.myMethod();
+   }
+}
